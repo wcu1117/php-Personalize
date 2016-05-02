991e1d71055fd3036de14c90d63b0a29c35964cb
wcu1117 <wcu789@qq.com>
2016-05-02 14:48:07 +0800

    php和mysql Web开发实例项目
    php和mysql Web开发实例项目
    巩固下知识和学习下github的用法

---
+++ login.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: hacker
+ * Date: 5/2/2016
+ * Time: 1:34 PM
+ * PHP bookmark login page
+ */
+require_once('bookmark_fns.php');
+do_html_header('');
+display_site_info();
+display_login_form();
+
+do_html_footer();
+?>
